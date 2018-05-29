af98606b2102855ccda0ea1872efe3068c509376
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2018-05-29 23:55:17 +0900

    コードの整理

--- Tile/Main.cpp
+++ Tile/Main.cpp
@@ -1,35 +1,32 @@
 ﻿//ドラッグアンドドロップでタイルを敷くプログラム
 #include <Siv3D.hpp>
 
-#define WINDOW_SIZE_X_G (960)
-#define WINDOW_SIZE_Y_G (540)
-#define TILE_SIZE_G (60)
+void tile()
+{
+	constexpr int32_t TILE_SIZE = 60;
+	const s3d::Array<s3d::DroppedFilePath> items = s3d::DragDrop::GetDroppedFilePaths();
+
+	for (const auto& item : items)
+	{
+		s3d::Print(item.path);
+
+		const s3d::Texture miniTexture(item.path);
+		for (int32_t i = 0; i < s3d::Window::Size().x; i += TILE_SIZE)
+			for (int32_t j = 0; j < s3d::Window::Size().y; j += TILE_SIZE)
+				miniTexture.resized(TILE_SIZE, TILE_SIZE).draw(i, j);
+
+	}
+}
 
 void Main()
 {
-	Window::SetTitle(U"gaccho tiler");
-	Window::Resize(WINDOW_SIZE_X_G, WINDOW_SIZE_Y_G);
+	constexpr int32_t WINDOW_SIZE_X = 960;
+	constexpr int32_t WINDOW_SIZE_Y = 540;
 
-	Array<s3d::DroppedFilePath> items;
+	s3d::Window::SetTitle(U"gaccho tiler");
+	s3d::Window::Resize(WINDOW_SIZE_X, WINDOW_SIZE_Y);
 
-	while (System::Update(false))
-	{
+	while (s3d::System::Update(false))
+		if (s3d::DragDrop::HasNewFilePaths()) tile();
 
-		if (DragDrop::HasNewFilePaths())
-		{
-			items = DragDrop::GetDroppedFilePaths();
-
-			for (const auto& item : items)
-			{
-				Print(item.path);
-
-				Texture miniTexture(item.path);
-				for (int i = 0; i < WINDOW_SIZE_X_G; i += TILE_SIZE_G) 
-					for (int j = 0; j < WINDOW_SIZE_Y_G; j += TILE_SIZE_G) 
-						miniTexture.resized(TILE_SIZE_G, TILE_SIZE_G).draw(i, j);
-
-			}
-		}
-
-	}
 }
